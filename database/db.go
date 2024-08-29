48f21e4401824d7fbd8b646c6ecd551149201e2b
joseph0x45 <zozozozeph@gmail.com>
2024-08-29 21:45:09 +0000

    add db connection and http server setup

--- database/db.go
+++ database/db.go
@@ -2,6 +2,10 @@ package database
 
 import "github.com/jmoiron/sqlx"
 
-func GetDBConnectionPool() *sqlx.DB {
-  return nil
+func GetDBConnectionPool(dbURL string) *sqlx.DB {
+	db, err := sqlx.Connect("postgres", dbURL)
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module server
 go 1.23.0
 
 require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/go-chi/cors v1.2.1
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
 )
